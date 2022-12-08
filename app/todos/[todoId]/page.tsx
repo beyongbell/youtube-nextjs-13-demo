a3e579dfdfdf89b910606fb029ab794867878baf
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2022-12-08 11:10:41 +0700

    Create Page

---
+++ app/todos/[todoId]/page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Todo } from '../../../typings'
+
+type PageProps = {
+    params: {
+        todoId: string
+    }
+}
+
+const fetchTodo = async (todoId: string) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`, { next: { revalidate: 60 }})
+    const todo: Todo = await res.json();
+    return todo;
+}
+
+async function TodoPage({ params: { todoId }}: PageProps) {
+    const todo = await fetchTodo(todoId)
+
+    return (
+        <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+            <p>
+                #{todo.id}: {todo.title}
+            </p>
+            <p> Completed: {todo.compoleted ? 'Yes' : 'No'} </p>
+            <p className="border-t border-black mt-5 text-right">
+                By User: {todo. userId}
+            </p>
+        </div>
+    )
+}
+
+export async function generateStaticParams() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+    const todos: Todo[] = await res.json();
+    const trimmedTodos = todos.slice(0, 10);
+    return trimmedTodos.map((todo) => ({ params: { todoId: todo.id.toString() }}))
+}
